c9863fdaa6a0b36916b15f07109333ff140e7875
injun2005 <77712212+injun2005@users.noreply.github.com>
2023-08-28 15:23:28 +0900

    Define

--- Assets/02.Scripts/Core/Define.cs
+++ Assets/02.Scripts/Core/Define.cs
@@ -1,18 +1,49 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
-public class Define : MonoBehaviour
+public static class Define 
 {
-    // Start is called before the first frame update
-    void Start()
+    public static bool ExistInHits(GameObject obj, object hits)
     {
-        
+        if (hits == null || !(hits is List<RaycastResult>))
+        {
+            Debug.LogError("Hits가 null이거나 타입이 맞지 않습니다");
+            return true;
+        }
+
+        foreach (RaycastResult hit in hits as List<RaycastResult>)
+        {
+            if (hit.gameObject == obj && (obj.transform.IsChildOf(hit.gameObject.transform) || hit.gameObject.transform.IsChildOf(obj.transform)))
+            {
+                return true;
+            }
+        }
+
+        return false;
     }
 
-    // Update is called once per frame
-    void Update()
+    public static bool ExistInFirstHits(GameObject obj, object hits)
     {
-        
+        if (hits == null || !(hits is List<RaycastResult>))
+        {
+            Debug.LogError("Hits가 null이거나 타입이 맞지 않습니다");
+            return true;
+        }
+
+        List<RaycastResult> rayList = hits as List<RaycastResult>;
+
+        if (rayList.Count == 0)
+        {
+            return false;
+        }
+
+        RaycastResult hit = rayList[0];
+        if (obj.transform.IsChildOf(hit.gameObject.transform) || hit.gameObject.transform.IsChildOf(obj.transform))
+        {
+            return true;
+        }
+        return false;
     }
 }
